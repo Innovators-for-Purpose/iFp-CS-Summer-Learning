849f4a77aa97dbb5b0fb920bf70a8a5c92e376cf
Muggin <magan.frx@gmail.com>
2026-07-07 14:42:42 -0400

    made the buttons scripts actually work, now they display informaiton when you click on them. Resized funky cat to fit in the profile.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function showAbout() {
     document.getElementById("Title").textContent="About Me";
-    document.getElementById("Description").textContent="Hello! My name is Magan.";
+    document.getElementById("Description").textContent="Hello! My name is Magan. I am an asipiring game developer who is interested in the story telling narrative aspect of games and the interesting complexities of game mechanics.";
 }
 
+function showSkills() {
+  document.getElementById("Title").textContent = "Skills";
+  document.getElementById("Description").innerHTML = "JavaScript<br>HTML<br>CSS<br>Python<br>";
+}
 function showExperience() {
-  document.getElementById("section-title").textContent = "Experience";
-  document.getElementById("description").textContent = "I have worked on projects with JavaScript and web design.";
+  document.getElementById("Title").textContent = "Experience";
+  document.getElementById("Description").textContent = "I have worked on projects with JavaScript and web design.";
 }
 
 function showInterests() {
-  document.getElementById("section-title").textContent = "Interests";
-  document.getElementById("description").textContent = "I like coding, game design, and learning new tools.";
+  document.getElementById("Title").textContent = "Interests";
+  document.getElementById("Description").textContent = "I like coding, game design, and learning new tools.";
 }
