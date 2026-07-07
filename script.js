8d248a8d6a7576f7c5ffb6983ef474debc956fcd
Muggin <magan.frx@gmail.com>
2026-07-07 14:05:29 -0400

    added js functions, and personalized the placeholder text

---
+++ script.js
@@ -0,0 +1,14 @@
+function showAbout() {
+    document.getElementById("Title").textContent="About Me";
+    document.getElementById("Description").textContent="Hello! My name is Magan.";
+}
+
+function showExperience() {
+  document.getElementById("section-title").textContent = "Experience";
+  document.getElementById("description").textContent = "I have worked on projects with JavaScript and web design.";
+}
+
+function showInterests() {
+  document.getElementById("section-title").textContent = "Interests";
+  document.getElementById("description").textContent = "I like coding, game design, and learning new tools.";
+}
