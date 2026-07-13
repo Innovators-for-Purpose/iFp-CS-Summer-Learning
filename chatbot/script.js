de9bd4e399a8c04f306a7a6ad8ef0fd04fb2189f
Muggin <magan.frx@gmail.com>
2026-07-13 15:13:35 -0400

    added chatbot features and added it to main page

---
+++ chatbot/script.js
@@ -0,0 +1,15 @@
+function openDialog() {
+    document.getElementById("dialog_box").style.display = "flex";
+}
+
+function showOptionOne() {
+    document.getElementById("response").textContent = "Option 1: Blah.";
+}
+
+function showOptionTwo() {
+    document.getElementById("response").textContent = "Option 2: Brah.";
+}
+
+function showOptionThree() {
+    document.getElementById("response").textContent = "Option 3: Bwah.";
+}
