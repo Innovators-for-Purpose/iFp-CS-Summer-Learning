7f773903ffa19717b5edd803ef39d24f9831a305
Muggin <magan.frx@gmail.com>
2026-07-15 13:53:14 -0400

    added border-radius of 3% to images

--- main page/script.js
+++ main page/script.js
@@ -27,7 +27,7 @@ function showInterests() {
 
 function showVideo() {
   document.getElementById("Title").textContent = "Cat Video";
-  document.getElementById("Description").innerHTML = '<iframe width="400" height="400" src="https://www.youtube.com/embed/sXB6_Foueik?si=3Jd9kUL0MZzi1EaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>';
+  document.getElementById("Description").innerHTML = '<iframe width="400" height="400" src="https://www.youtube.com/embed/sXB6_Foueik?si=3Jd9kUL0MZzi1EaI" title="cat eating then looking at the camera meme" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
 }
 
 function showGenius() {
