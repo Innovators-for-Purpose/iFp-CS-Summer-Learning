3f385322bace58dcc9234fadcd8fb3f88b02b56d
Avry M <visualiscool1214@gmail.com>
2026-07-15 10:23:47 -0400

    Working on responses in the chatboat

---
+++ ChatBot/script.js
@@ -0,0 +1,12 @@
+function openDialogue() {
+   document.getElementById("dialogue-box").style.display="block";
+}
+function showOptionOne() {
+  document.getElementById("response").textContent = "Im Verity! your personal helper friend, ask me anything, i know everything.";
+}
+function showOptionTwo() {
+  document.getElementById("response").textContent = "Im doing 'mind blowingly' good!";
+}
+function showOptionThree() {
+  document.getElementById("response").textContent = "Avrys a animator and artist, hes the one who made this website!";
+}
