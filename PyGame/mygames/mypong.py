03ad4f7f0b1722ed13098eaea46da5142ed448ea
30eabay-netizen <30eabay@cpsd.us>
2026-09-24 18:58:21 -0400

    Pong game finished, Snake creatioin

--- PyGame/mygames/mypong.py
+++ PyGame/mygames/mypong.py
@@ -1,27 +1,33 @@
 import pygame
 import sys
 
-pygame.init()
 
 #Variables
 WIDTH, HEIGHT = 900, 600
 FPS = 60
 WHITE = (255, 255, 255)
+ORANGE = (255, 165, 0)
 BLACK = (0, 0, 0)
 PADDLE_SPEED = 6
 BALL_SPEED_X = 5
 BALL_SPEED_Y = 4
 
+BLUE = (48, 105, 152)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+MAROON = (128, 0, 0)
+ROSE = (255, 120, 127)
 
 PADDLE_WIDTH = 15
 PADDLE_HEIGHT = 110
 BALL_SIZE = 18
 
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+left_score = 0
+right_score = 0
+
+pygame.init()
 pygame.display.set_caption("My Pong")
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 
@@ -56,6 +62,7 @@ font = pygame.font.Font(None, 60)
 
 
 def reset_ball(direction):
+    global ball_speed_x, ball_speed_y
     ball.center = (WIDTH // 2, HEIGHT // 2)
     ball_speed_x = direction * BALL_SPEED_X
     ball_speed_y = BALL_SPEED_Y
@@ -63,68 +70,62 @@ def reset_ball(direction):
 
 #MAIN FUNCTION!!!!
 
-def main(window):
 
-    while True:
-        #codeeeeee
-        clock.tick(60)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_s] and left_paddle.top < HEIGHT: left_paddle.y -= PADDLE_SPEED
-        if keys[pygame.K_w] and left_paddle.top > 0:left_paddle.y += PADDLE_SPEED
-        if keys[pygame.K_UP] and right_paddle.top > 0:right_paddle.y -= PADDLE_SPEED
-        if keys[pygame.K_DOWN] and right_paddle.bottom < HEIGHT:right_paddle.y += PADDLE_SPEED
-
-        ball.x += ball_speed_x
-        ball.y += ball_speed_y
-
-        if ball.top <= 0 or ball.bottom >= HEIGHT:
-            ball_speed_y *= -1
+while True:
+    #codeeeeee
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
 
-        if ball.colliderect(left_paddle) and ball_speed_x < 0:
-            ball_speed_x *= -1
-            offset = (ball.centery - left_paddle.centery) / (PADDLE_HEIGHT / 2) 
-            ball_speed_y = offset * 6
-        if ball.colliderect(right_paddle) and ball_speed_x > 0:
-            ball_speed_x *= -1
-            offset = (ball.centery - right_paddle.centery) / (PADDLE_HEIGHT / 2) 
-            ball_speed_y = offset * 6
+            
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and left_paddle.top > 0:left_paddle.y -= PADDLE_SPEED
+    if keys[pygame.K_s] and left_paddle.bottom < HEIGHT:left_paddle.y += PADDLE_SPEED
+    if keys[pygame.K_UP] and right_paddle.top > 0:right_paddle.y -= PADDLE_SPEED
+    if keys[pygame.K_DOWN] and right_paddle.bottom < HEIGHT:right_paddle.y += PADDLE_SPEED
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        ball_speed_y *= -1
 
 
-        left_score = 0
-        right_score = 0
-
-        if ball.left <= 0:
-            right_score += 1
-            reset_ball(1)
-        elif ball.right >= WIDTH:
-            left_score += 1
-            reset_ball(-1)
-
-
-        window.fill(BLACK)
-
-        for y in range(0, HEIGHT, 20):
-            pygame.draw.rect(window, WHITE, (WIDTH // 2 - 2, y, 4, 10))
+    if ball.colliderect(left_paddle) and ball_speed_x < 0:
+        ball_speed_x *= -1
+        offset = (ball.centery - left_paddle.centery) / (PADDLE_HEIGHT / 2) 
+        ball_speed_y = offset * 6
+    if ball.colliderect(right_paddle) and ball_speed_x > 0:
+        ball_speed_x *= -1
+        offset = (ball.centery - right_paddle.centery) / (PADDLE_HEIGHT / 2) 
+        ball_speed_y = offset * 6
 
 
-        pygame.draw.rect(window, WHITE, left_paddle)
-        pygame.draw.rect(window, WHITE, right_paddle)
-        pygame.draw.ellipse(window, WHITE, ball)
 
-        score_text = font.render(f"{left_score}  {right_score}", True, WHITE)
-        window.blit(score_text, (WIDTH // 2 - score_text.width() // 2, 20))
+    if ball.left <= 0:
+        right_score += 1
+        reset_ball(1)
+    elif ball.right >= WIDTH:
+        left_score += 1
+        reset_ball(-1)
 
-        pygame.display.flip()
-        clock.tick(FPS)
 
-#if __name__ == "__main__":
-    pygame.init()
-    window = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("My Pong")
-    main(window)
-    pygame.quit()
+    window.fill(WHITE)
+
+    for y in range(0, HEIGHT, 20):
+        pygame.draw.rect(window, MAROON, (WIDTH // 2 - 2, y, 4, 10))
+
+
+    pygame.draw.rect(window, BLUE, left_paddle)
+    pygame.draw.rect(window, ROSE, right_paddle)
+    pygame.draw.ellipse(window, ORANGE, ball)
+
+    score_text = font.render(f"{left_score}  {right_score}", True, MAROON)
+    window.blit(score_text, (WIDTH // 2 - score_text.get_width() // 2, 20))
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+
